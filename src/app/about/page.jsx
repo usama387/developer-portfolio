739b29497f80d80ea6370953ec3c6b3ef03222fa
usama387 <128306049+usama387@users.noreply.github.com>
2024-02-19 15:07:36 +0500

    Added email and svg on contact Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,24 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { motion, useScroll, useInView} from "framer-motion";
+import AboutPageSvg from "@/components/Svg";
 
 const AboutPage = () => {
+
+  const containerRef = useRef();
+
+  // 2.1 Calculating Scroll by passing in container
+  const {scrollYProgress} = useScroll({container:containerRef});
+  // when the container start the scroll position is 0.5 and 1 at the end using this i will give rotation animation
+
+  //3.2) checking if the skills section in viewport
+  const skillRef = useRef();
+  
+  const isSkillRefInView = useInView(skillRef, {margin:"-100px"});
+
+ 
+
+
   return (
     // 1)First div for animation when this page is rendered
     <motion.div
@@ -11,14 +27,14 @@ const AboutPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      {/* 2)Container */}
-      <div>
+      {/* 2.1)Container that holds Text and Image */}
+      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
         {/*3) Parent div of Text Container of About Page  */}
-        <div className=" flex flex-col gap-24 p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 md:gap-32 lg:gap-48 xl:gap-64">
+        <div className=" flex flex-col gap-24 p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:h-1/2">
           {/* 3.1) Biography Container */}
           <div className="flex flex-col gap-12 justify-center">
-            <h1>BIOGRAPHY</h1>
-            <p>
+            <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
+            <p className="text-lg">
               Welcome to my digital sanctuary, where pixels dance and code
               weaves tales of innovation. As a Next.js web developer, I invite
               you to embark on a journey through my portfolio, a testament to my
@@ -53,17 +69,177 @@ const AboutPage = () => {
                 strokeWidth="1"
               ></path>
             </motion.svg>
-            {/* 1:17:35 */}
           </div>
           {/* 3.2 Skills Container*/}
-          <div>SKILLS</div>
+          <div className="flex flex-col gap-12 justify-center" ref={skillRef}>
+            <motion.h1 initial={{x:"-300px"}} animate={isSkillRefInView ? {x:0} : {}} transition={{delay:0.2}} className="font-bold text-2xl">SKILLS</motion.h1>
+            {/* SKILLS LIST &&  note that flex wrap adds items to new line if it does not fit in the container in rows */}
+            <div className="flex flex-wrap gap-4">
+              <motion.div initial={{x:"-300px"}} animate={isSkillRefInView ? {x:0} : {}} transition={{delay:0.2}} className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                JavaScript
+              </motion.div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                HTML5
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                CSS3
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                TAILWIND CSS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                REACT JS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                NEXT JS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                MONGODB
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                SANITY
+              </div>
+            </div>
+            {/* SKILL SCROLL SVG*/}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
+          </div>
           {/* 3.3 Experience Container */}
-          <div>EXPERIENCE</div>
+          <div className="flex flex-col gap-12 justify-center pb-48">
+            <h1 className="font-bold text-2xl">EXPERIENCE</h1>
+          </div>
+          {/* EXPERIENCE LIST */}
+          <div>
+            {/* LIST ITEM */}
+            <div className="flex justify-between h-48">
+              {/* LEFT */}
+              <div className="w-1/3">
+                {/* JOB TITLE */}
+                <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg ">
+                  JavaScript Engineer
+                </div>
+                {/* JOB DESC */}
+                <div className="p-2 text-sm italic ">
+                  Learning everyday making projects to gain hands on experience
+                  & skills
+                </div>
+                {/* JOB DATE */}
+                <div className="p-3 text-red-400 text-sm font-semibold">
+                  2024-Present
+                </div>
+                {/* JOB COMPANY */}
+                {/* <div className="p-1 rounded bg-white text-sm font-semibold w-fit">META</div> */}
+              </div>
+              {/* CENTER */}
+              <div className="w-1/6">
+                {/* LINE */}
+                <div className="w-1 h-full bg-gray-600 rounded relative">
+                  <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2">
+                    {/*LINE CIRCLE */}
+                  </div>
+                </div>
+              </div>
+              {/* RIGHT */}
+              <div className="w-1/3"></div>
+            </div>
+          </div>
+          {/* EXPERIENCE LIST */}
+          <div>
+            {/* LIST ITEM */}
+            <div className="flex justify-between h-48">
+              {/* LEFT */}
+              <div className="w-1/3"></div>
+              {/* CENTER */}
+              <div className="w-1/6">
+                {/* LINE */}
+                <div className="w-1 h-full bg-gray-600 rounded relative">
+                  <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2">
+                    {/*LINE CIRCLE */}
+                  </div>
+                </div>
+              </div>
+              {/* RIGHT */}
+              <div className="w-1/3">
+                {/* JOB TITLE */}
+                <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg ">
+                  JavaScript Engineer
+                </div>
+                {/* JOB DESC */}
+                <div className="p-2 text-sm italic ">
+                  Learning everyday making projects to gain hands on experience
+                  & skills
+                </div>
+                {/* JOB DATE */}
+                <div className="p-3 text-red-400 text-sm font-semibold">
+                  2024-Present
+                </div>
+                {/* JOB COMPANY */}
+                {/* <div className="p-1 rounded bg-white text-sm font-semibold w-fit">META</div> */}
+              </div>
+            </div>
+          </div>
+          {/* EXPERIENCE LIST */}
+          <div>
+            {/* LIST ITEM */}
+            <div className="flex justify-between h-48">
+              {/* LEFT */}
+              <div className="w-1/3">
+                {/* JOB TITLE */}
+                <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg ">
+                  JavaScript Engineer
+                </div>
+                {/* JOB DESC */}
+                <div className="p-2 text-sm italic ">
+                  Learning everyday making projects to gain hands on experience
+                  & skills
+                </div>
+                {/* JOB DATE */}
+                <div className="p-3 text-red-400 text-sm font-semibold">
+                  2024-Present
+                </div>
+                {/* JOB COMPANY */}
+                {/* <div className="p-1 rounded bg-white text-sm font-semibold w-fit">META</div> */}
+              </div>
+              {/* CENTER */}
+              <div className="w-1/6">
+                {/* LINE */}
+                <div className="w-1 h-full bg-gray-600 rounded relative">
+                  <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2">
+                    {/*LINE CIRCLE */}
+                  </div>
+                </div>
+              </div>
+              {/* RIGHT */}
+              <div className="w-1/3"></div>
+            </div>
+          </div>
         </div>
-        {/* 4) Image Container */}
-        <div className="hidden"></div>
+        {/* 4) SVG  CONTAINER */}
+        <div className="hidden lg:block w-1/3 xl:h-1/2 sticky top-0 z-30">
+          {/* 2.1 Passing in as props scrollYProgress */}
+          <AboutPageSvg scrollYProgress={scrollYProgress} />
+        </div>
       </div>
-      About Page
     </motion.div>
   );
 };
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,116 @@
 "use client";
-import React from 'react';
-import {motion} from "framer-motion";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { motion } from "framer-motion";
 
 const ContactPage = () => {
-  return (
-    <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration: 1}}>
-    Contact Page
-  </motion.div>
-  )
-}
+  // 2.1 Text that will be displayed on page
+  const text = "Write me an email and let's connect!";
 
-export default ContactPage
+  // 3.1) useState to manage success of message
+  const [success, setSuccess] = useState(false);
+
+  // 3.1) useState to manage error of message
+  const [error, setError] = useState(false);
+
+  // my_service_id from email js
+  // service_whvyj73
+
+  // 3.1) Configuration for email/js
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(false);
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+        }
+      )
+      .then(
+        () => {
+          setSuccess(true);
+          form.current.reset();
+        },
+        (error) => {
+          setError(true);
+        }
+      );
+  };
+
+  return (
+    // 1)Adding framer motion to the most parent div fro smooth opening of page
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      {/* 2.1) */}
+      <div className="h-full flex flex-col lg:flex-row   px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* 2.1 Text Container*/}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+        </div>
+        {/* 3.1 Form Container */}
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          className="h-1/2 lg:h-full lg:w-1/2 rounded-xl text-xl flex flex-col gap-8 justify-center p-24 bg-slate-300"
+        >
+          {/* 3.1 Contents of form*/}
+          <span>Dear Usama,</span>
+          <textarea
+            rows={6}
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            name="user_message"
+          />
+          <span>My email address is:</span>
+          <input
+            name="user_email"
+            type="text"
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+          />
+          <span>Regards</span>
+          <button className="bg-[#533FD7] text-[#54F3A6] rounded font-semibold p-4">
+            Send
+          </button>
+          {success && (
+            <span className="text-green-800 font-semibold">
+              Message sent successfully
+            </span>
+          )}
+          {error && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
+        </form>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ContactPage;
